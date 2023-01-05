160c124e8fc15c3e6684bf6fca455103677fc43d
durgaprasadmanepalli <121545125+durgaprasadmanepalli@users.noreply.github.com>
2023-01-05 10:56:03 +0530

    Add files via upload

---
+++ prasadaxis1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class prasadaxis1 {
+    String name;
+    int id;
+    String company;
+    String place;
+    protected int age;
+    private String pos;
+    public String position;
+
+    public void createUser(prasadaxis1 obj) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name:");
+        obj.name = sc.next();
+        System.out.println("Enter id :");
+        obj.id = sc.next();
+        System.out.println("Enter company :");
+        obj.company = sc.next();
+        System.out.println("Enter age: ");
+        obj.age = sc.nextInt();
+        System.out.println("Enter pos: ");
+        obj.pos = sc.next();
+
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("User id: " + id);
+        System.out.println("Company: " + company);
+        System.out.println("Age: " + age);
+        System.out.println("Pos: " + pos);
+    }
+
+    public static void main(String[] args) {
+        prasadaxis1 obj = new prasadaxis1();
+        obj.createUser(obj);
+        obj.display();
+
+    }
+
+}
