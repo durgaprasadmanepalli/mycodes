284ae874bcd9df1e7103d08429605b5277a89cbd
durgaprasadmanepalli <121545125+durgaprasadmanepalli@users.noreply.github.com>
2023-01-06 18:06:47 +0530

    Add files via upload

---
+++ Retail.java
@@ -0,0 +1,8 @@
+public interface Retail {
+    float getTotalcost(float tax, float cost, float number);
+
+    float getTotalextendedcost(float cosmetics, float perfume, float clothes);
+
+    void getTotalDiscount(float totalcost);
+
+}
