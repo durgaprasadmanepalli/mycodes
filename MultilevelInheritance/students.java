3da0260384d10ab17cb0982e455c2e17600611b5
durgaprasadmanepalli <121545125+durgaprasadmanepalli@users.noreply.github.com>
2023-01-10 11:51:13 +0530

    Add files via upload

---
+++ MultilevelInheritance/students.java
@@ -0,0 +1,29 @@
+public class students {
+    
+    String name;
+    int age;
+    String place;
+    String collage;
+   
+    couunt = 0;
+    public void addstudent(){ 
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter your name");
+       name = sc.next();
+       System.out.println("Enter your age");
+       age = sc.nextInt();
+       System.out.println("Enter your place");
+       place = sc.next();
+       System.out.println("Enter your collage");
+       collage = sc.next();
+       s
+
+
+    }
+
+
+ 
+
+    }
+
+}
